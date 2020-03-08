ab9fb71314318d901fd9c8614dbc97f5556fc59c
Sudarshan <sudarshan.java.p@gmail.com>
2020-03-08 13:33:00 +0800

    Base Release

---
+++ src/test/java/com/star/sud/SpringBootRestServiceFiltering/SpringBootRestServiceFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.sud.SpringBootRestServiceFiltering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceFilteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
